9e859457f24968d3883c8f93642895e1b412e9fb
Arnav Doshi <asdoshi1@uci.edu>
2026-02-23 18:39:15 -0800

    Train.py helper methods
    Can calculate returns, advantage, loss

---
+++ src/ppo/train.py
@@ -0,0 +1,56 @@
+import network as net
+import src.envs.robot_env as robenv
+from torch import *
+import torch.nn as nn
+import torch.nn.functional as f
+import matplotlib.pyplot as plt
+
+env = robenv.RobotEnv(16, 0)
+
+#actor critic class - will remove once officially implemented
+
+class ActorCritic(nn.Module):
+    def __init__(self, actor, critic):
+        super().__init__()
+        self.actor = actor
+        self.critic = critic
+
+    
+    def forward(self, state):
+        action_pred = self.actor(state)
+        value_pred = self.critic(state)
+        return action_pred, value_pred
+    
+
+
+agent = ActorCritic(net.Actor, net.Critic)
+
+def calculate_returns(rewards, discount_factor):
+    returns = []
+    cumulative_reward = 0
+    for r in reversed(rewards):
+        cumulative_reward =  r + cumulative_reward * discount_factor
+        returns.insert(0,cumulative_reward)
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / returns.std()
+    return returns
+
+def calculate_advantages(returns, values):
+    advantages = returns - values
+    advantages = (advantages - advantages.mean())/ advantages.std()
+    return advantages
+
+def calculate_surrogate_loss(actions_log_probability_old, actions_log_probability_new, epsilon, advantages):
+    advantages = advantages.detach()
+    policy_ratio = (actions_log_probability_new - actions_log_probability_old).exp()
+    surrogate_loss1 = policy_ratio * advantages
+    surrogate_loss2= torch.clamp(policy_ratio, min=1.0-epsilon, max=1.0+epsilon) * advantages
+    surrogate_loss=  torch.min(surrogate_loss1,surrogate_loss2)
+    return surrogate_loss
+
+
+def calculate_losses(surrogate_loss, entropy, entropy_coefficient, returns, value_pred):
+    entropy_bonus = entropy_coefficient * entropy
+    policy_loss=  -(surrogate_loss + entropy_bonus ).sum()
+    value_loss=  f.smooth_l1_loss(returns, value_pred).sum()
+    return policy_loss, value_loss
